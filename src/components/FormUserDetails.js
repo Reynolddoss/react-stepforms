92615028c8e76c62fc502406e687b6f788b76ff6
reynold <reynold.da1@iiitmk.ac.in>
2020-07-14 12:39:14 +0530

    radio buttons

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -4,6 +4,7 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Grid from '@material-ui/core/Grid'
+import { Button,Radio,RadioGroup,FormControlLabel,FormControl,FormLabel } from '@material-ui/core';
 
 class FormUserDetails extends Component {
     continueBtn = e =>{
@@ -13,7 +14,9 @@ class FormUserDetails extends Component {
 
     render() {
         const {values,handleChange} = this.props;
-        const styles = {button:{margin:15}}
+        const styles = {button:{margin:15},
+                        radio:{padding:'0px',marginTop:'10px'},
+                        radioLabel:{textAlign:'left'}}
 
 
         return (
@@ -37,9 +40,49 @@ class FormUserDetails extends Component {
                                     onChange={handleChange('lastName')}
                                     />
                         <br/>
+                        <FormControl component="fieldset">
+                                <FormLabel component="legend" style={styles.radioLabel}>Logo Image</FormLabel>
+                                <RadioGroup row aria-label="position" name="position" defaultValue="Uploaded">
+                                    
+                                    <FormControlLabel value="Uploaded"
+                                                    control={<Radio color="primary" style={styles.radio}/>}
+                                                    label="Uploaded"
+                                                    labelPlacement="bottom"
+                                    />
+                                    <FormControlLabel value="Custom"
+                                                    control={<Radio color="primary" style={styles.radio} />}
+                                                    label="Custom"
+                                                    labelPlacement="bottom"
+                                    />
+                                    
+                                </RadioGroup>
+                        </FormControl>
+                        <br/>
+                        <TextField 
+                                    hintText="Enter Last Name"
+                                    floatingLabelText = "Last Name"
+                                    defaultValue={values.lastName} 
+                                    onChange={handleChange('lastName')}
+                                    />
+                        <br/>
+
+                       <RaisedButton
+                                containerElement='label'>
+                                <input type="file" />
+                        </RaisedButton>
+                        <Button variant="contained" component="label">Upload</Button>
+                        <br/> 
                         <TextField 
                                     hintText="Enter Email"
                                     floatingLabelText = "email"
+                                    defaultValue={values.email} 
+                                    onChange={handleChange('email')}
+                                    />
+                    <br/>
+                    <TextField         
+                                    rows = {4}
+                                    hintText="Description"
+                                    floatingLabelText = "Description"
                                     defaultValue={values.email} 
                                     onChange={handleChange('email')}
                                     />
